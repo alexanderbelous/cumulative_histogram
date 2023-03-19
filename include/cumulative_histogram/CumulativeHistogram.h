1aa73b3e837dc2a7fd34f9d1c8eb9f61d57bc096
Alexander Belous <alexander.belous@outlook.com>
2023-03-19 05:32:19 +0100

    Store the elements and the auxiliary counters in the same array.

--- include/cumulative_histogram/CumulativeHistogram.h
+++ include/cumulative_histogram/CumulativeHistogram.h
@@ -19,7 +19,6 @@ class CumulativeHistogram {
   constexpr CumulativeHistogram() noexcept = default;
 
   // Constructs a cumulative histogram for N elements.
-  // TODO: time complexity?
   explicit CumulativeHistogram(std::size_t num_elements);
 
   // Sets the values of all elements to 0.
@@ -56,90 +55,110 @@ class CumulativeHistogram {
   // Time complexity: O(1).
   constexpr T totalSum() const noexcept;
 
+  // Find the first element k, for which partialSum(k) is not less than the specified value.
+  // Time complexity: O(log2(N)).
+  // TODO: implement.
+
  private:
+  // Returns the total number of nodes in the auxiliary tree for
+  // CumulativeHistogram with the specified number of elements.
   static constexpr std::size_t countNodesInTree(std::size_t num_elements) noexcept;
 
   template<bool Mutable>
-  class TreeView {
-   public:
-     using value_type = std::conditional_t<Mutable, T, const T>;
-     using reference = value_type&;
+  class TreeView;
 
-     constexpr TreeView(std::span<value_type> nodes,
-                        std::size_t element_first,
-                        std::size_t element_last) noexcept:
-       nodes_(nodes),
-       element_first_(element_first),
-       element_last_(element_last)
-     {}
-
-     constexpr bool empty() const noexcept {
-       return nodes_.empty();
-     }
-
-     constexpr std::size_t pivot() const noexcept {
-       return element_first_ + (element_last_ - element_first_) / 2;
-     }
-
-     reference root() const {
-       return nodes_.front();
-     }
-
-     constexpr TreeView leftChild() const{
-       const std::size_t num_nodes_left = nodes_.size() / 2;
-       return TreeView(nodes_.subspan(1, num_nodes_left), element_first_, pivot());
-     }
-
-     constexpr TreeView rightChild() const {
-       const std::size_t num_nodes_left = nodes_.size() / 2;
-       const std::size_t num_nodes_right = (nodes_.size() - 1) / 2;
-       return TreeView(nodes_.subspan(1 + num_nodes_left, num_nodes_right), pivot() + 1, element_last_);
-     }
-
-   private:
-    std::span<value_type> nodes_;
-    std::size_t element_first_;
-    std::size_t element_last_;
-  };
-
+  // Array of N+M counters, where the first N are the actual elements,
+  // and the remaining M counters are the nodes of the implicit tree.
   std::vector<T> data_;
-  // Tree data.
-  // The first element is the root. The root stores the sum of elements from its left branch.
-  // The left branch is a tree representing elements [0; (N-1)/2].
-  // The right branch is a tree representing elements [(N+1)/2; N).
-  //
-  // The tree is an implicit data structure:
-  //   * We know the total number of nodes in the tree from the formula in countNodesInTree().
-  //   * The tree is balanced: for any node the numbers of nodes in the left and right branches
-  //     differ at most by 1.
-  //   * Therefore, we can store it in a plain array like this:
-  // Number of elements = 11; total number of nodes = 6
-  //         _ n0 __
-  //        /       \
-  //       n1        n4
-  //     /    \      /
-  //   n2     n3     n5
-  // .......................implicit nodes........
-  //  /  \    / \    /  \    \
-  // x0  x2  x3  x5 x6  x8   x9
-  //  \       \      \        \
-  //   x1      x4    x7      x10
-  //
-  // Nodes:
-  // +-------------------+----------+---------+---------+----------+---------+
-  // |    n0             |    n1    |    n2   |    n3   |    n4    |    n5   |
-  // +-------------------+----------+---------+---------+----------+---------+
-  // | x0+x1+x2+x3+x4+x5 | x0+x1+x2 | x0 + x1 | x3 + x4 | x6+x7+x8 | x6 + x7 |
-  // +-------------------+----------+---------+---------+----------+---------+
-  // Any partial sum can be computed by going from the root of the tree to the leaf:
-  // +------------------------------------------------------------------------------------------+
-  // |  s0  |  s1  |  s2  |  s3   |  s4   |  s5  |  s6   |  s7   |  s8   |  s9      |  s10      |
-  // +------------------------------------------------------------------------------------------+
-  // |  x0  |  n2  |  n1  | n1+x3 | n1+n3 |  n0  | n0+x6 | n0+n5 | n0+n4 | n0+n4+x9 | total_sum |
-  // +------------------------------------------------------------------------------------------+
-  std::vector<T> nodes_;
-
+  // The number of elements N.
+  std::size_t num_elements_;
+  // Sum of all elements [0; N).
   T total_sum_ = {};
+};
+
+// ----==== Implementation ====----
+
+// CumulativeHistogram stores auxiliary counters for sums of certain elements.
+// These counters are updated whenever the respective elements are modified -
+// this is why CumulativeHistogram::increment() has O(log2(N)) time complexity.
+// However, having these sums precomputed also allows efficient computation of the partial sums.
+//
+// These additional counters form an implicit binary tree:
+// * The main tree represents all elements [0; N).
+// * The left branch is a tree representing elements [0; (N-1)/2].
+// * The right branch is a tree representing elements [(N+1)/2; N).
+// * The tree is balanced: for any node the numbers of nodes in the left and right branches
+//   differ at most by 1.
+// * The nodes of the tree are stored as a plain array: [root, left0, left1, ... leftM, right0, right1, ... rightK].
+// * The root stores the sum of elements from its left branch.
+// * The tree only stores sums of elements, not the individual elements.
+//
+// For example, for N=11, the total number of nodes is countNodesInTree(11) = 6:
+//         _ n0 __
+//        /       \
+//       n1        n4
+//     /    \      /
+//   n2     n3     n5
+// ......imaginary nodes......
+//  /  \    / \    /  \    \
+// x0  x2  x3  x5 x6  x8   x9
+//  \       \      \        \
+//   x1      x4    x7      x10
+//
+// Nodes:
+// +-------------------+----------+---------+-------+----------+---------+
+// |    n0             |    n1    |   n2    |   n3  |    n4    |    n5   |
+// +-------------------+----------+---------+-------+----------+---------+
+// | x0+x1+x2+x3+x4+x5 | x0+x1+x2 | x0+x1   | x3+x4 | x6+x7+x8 | x6 + x7 |
+// +-------------------+----------+---------+-------+----------+---------+
+// Any partial sum can be computed by going from the root of the tree to the leaf:
+// +------------------------------------------------------------------------------------------+
+// |  s0  |  s1  |  s2  |  s3   |  s4   |  s5  |  s6   |  s7   |  s8   |  s9      |  s10      |
+// +------------------------------------------------------------------------------------------+
+// |  x0  |  n2  |  n1  | n1+x3 | n1+n3 |  n0  | n0+x6 | n0+n5 | n0+n4 | n0+n4+x9 | total_sum |
+// +------------------------------------------------------------------------------------------+
+template<class T>
+template<bool Mutable>
+class CumulativeHistogram<T>::TreeView {
+public:
+  using value_type = std::conditional_t<Mutable, T, const T>;
+  using reference = value_type&;
+
+  constexpr TreeView(std::span<value_type> nodes,
+    std::size_t element_first,
+    std::size_t element_last) noexcept :
+    nodes_(nodes),
+    element_first_(element_first),
+    element_last_(element_last)
+  {}
+
+  constexpr bool empty() const noexcept {
+    return nodes_.empty();
+  }
+
+  constexpr std::size_t pivot() const noexcept {
+    return element_first_ + (element_last_ - element_first_) / 2;
+  }
+
+  reference root() const {
+    return nodes_.front();
+  }
+
+  constexpr TreeView leftChild() const {
+    const std::size_t num_nodes_left = nodes_.size() / 2;
+    return TreeView(nodes_.subspan(1, num_nodes_left), element_first_, pivot());
+  }
+
+  constexpr TreeView rightChild() const {
+    const std::size_t num_nodes_left = nodes_.size() / 2;
+    const std::size_t num_nodes_right = (nodes_.size() - 1) / 2;
+    return TreeView(nodes_.subspan(1 + num_nodes_left, num_nodes_right), pivot() + 1, element_last_);
+  }
+
+private:
+  std::span<value_type> nodes_;
+  std::size_t element_first_;
+  std::size_t element_last_;
 };
 
 template<class T>
@@ -166,33 +185,41 @@ constexpr std::size_t CumulativeHistogram<T>::countNodesInTree(std::size_t num_e
 
 template<class T>
 CumulativeHistogram<T>::CumulativeHistogram(std::size_t num_elements):
-  data_(num_elements),
-  nodes_(countNodesInTree(num_elements))
+  data_(num_elements + countNodesInTree(num_elements)),
+  num_elements_(num_elements)
 {}
 
 template<class T>
+void CumulativeHistogram<T>::setZero() {
+  std::fill(data_, T{});
+}
+
+template<class T>
 constexpr std::size_t CumulativeHistogram<T>::numElements() const noexcept {
-  return data_.size();
+  return num_elements_;
 }
 
 template<class T>
 constexpr std::span<const T> CumulativeHistogram<T>::elements() const noexcept {
-  return std::span<const T>{data_};
+  return std::span<const T>{data_.data(), num_elements_};
 }
 
 template<class T>
 const T& CumulativeHistogram<T>::element(std::size_t k) const {
-  return data_.at(k);
+  if (k >= num_elements_) {
+    throw std::out_of_range("CumulativeHistogram::element(): k is out of range.");
+  }
+  return data_.data()[k];
 }
 
 template<class T>
 void CumulativeHistogram<T>::increment(std::size_t k, const T& value) {
-  const std::size_t n = data_.size();
-  if (k >= n) {
+  if (k >= num_elements_) {
     throw std::out_of_range("CumulativeHistogram::increment(): k is out of range.");
   }
-  // Tree representing the elements [0; n).
-  TreeView<true> tree(nodes_, 0, n - 1);
+  // Tree representing the elements [0; N).
+  const std::span<T> nodes = std::span<T>{data_}.subspan(num_elements_);
+  TreeView<true> tree(nodes, 0, num_elements_ - 1);
   while (!tree.empty()) {
     // Check whether the element k is in the left or the right branch.
     if (k <= tree.pivot()) {
@@ -211,16 +238,16 @@ void CumulativeHistogram<T>::increment(std::size_t k, const T& value) {
 
 template<class T>
 T CumulativeHistogram<T>::partialSum(std::size_t k) const {
-  const std::size_t n = data_.size();
-  if (k >= n) {
-    throw std::out_of_range("CumulativeHistogram::increment(): k is out of range.");
+  if (k >= num_elements_) {
+    throw std::out_of_range("CumulativeHistogram::partialSum(): k is out of range.");
   }
-  if (k == n - 1) {
+  if (k == num_elements_ - 1) {
     return total_sum_;
   }
   T result {};
-  // Tree representing the elements [0; n).
-  TreeView<false> tree(nodes_, 0, n - 1);
+  // Tree representing the elements [0; N).
+  const std::span<const T> nodes = std::span<const T>{data_}.subspan(num_elements_);
+  TreeView<false> tree(nodes, 0, num_elements_ - 1);
   while (!tree.empty()) {
     // The root of the tree stores the sum of all elements [first; middle].
     const std::size_t middle = tree.pivot();
