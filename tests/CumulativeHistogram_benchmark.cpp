b044367978e2be9d753adcb229796c72c1fee824
Alexander Belous <alexander.belous@outlook.com>
2023-10-05 05:37:43 +0200

    Remove manual loops in benchmarks.
    The framework already measures the performance of operations in loops; no need to add nested loops.

--- tests/CumulativeHistogram_benchmark.cpp
+++ tests/CumulativeHistogram_benchmark.cpp
@@ -44,32 +44,28 @@ class ArrayOfPrefixSums {
 };
 
 void BM_ArrayOfPrefixSumsIncrement(benchmark::State& state) {
-  constexpr std::size_t kNumOperations = 1024;
   const std::size_t num_elements = static_cast<std::size_t>(state.range(0));
   ArrayOfPrefixSums<std::uint32_t> histogram(num_elements);
   std::mt19937 gen;  // mersenne_twister_engine seeded with some default value.
   std::uniform_int_distribution<std::size_t> distribution{ 0, num_elements - 1 };
-  for (auto _ : state)
-    for (std::size_t iteration = 0; iteration < kNumOperations; ++iteration) {
-      // Increment a random element by 1.
-      const std::size_t i = distribution(gen);
-      histogram.increment(i, 1);
-    }
+  for (auto _ : state) {
+    // Increment a random element by 1.
+    const std::size_t i = distribution(gen);
+    histogram.increment(i, 1);
+  }
 }
 BENCHMARK(BM_ArrayOfPrefixSumsIncrement)->Range(8, 32 << 10);
 
 void BM_CumulativeHisogramIncrement(benchmark::State& state) {
-  constexpr std::size_t kNumOperations = 1024;
   const std::size_t num_elements = static_cast<std::size_t>(state.range(0));
   CumulativeHistogram<std::uint32_t> histogram(num_elements);
   std::mt19937 gen;  // mersenne_twister_engine seeded with some default value.
   std::uniform_int_distribution<std::size_t> distribution{ 0, num_elements - 1 };
-  for (auto _ : state)
-    for (std::size_t iteration = 0; iteration < kNumOperations; ++iteration) {
-      // Increment a random element by 1.
-      const std::size_t i = distribution(gen);
-      histogram.increment(i, 1);
-    }
+  for (auto _ : state) {
+    // Increment a random element by 1.
+    const std::size_t i = distribution(gen);
+    histogram.increment(i, 1);
+  }
 }
 BENCHMARK(BM_CumulativeHisogramIncrement)->Range(8, 32 << 10);
 
