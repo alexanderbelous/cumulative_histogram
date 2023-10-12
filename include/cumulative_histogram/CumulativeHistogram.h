8871f19a42fde6ebc91f6d5d433ed1747c158661
Alexander Belous <alexander.belous@outlook.com>
2023-10-12 18:38:05 +0200

    Code refactoring for CompressedPath::pushBack().

--- include/cumulative_histogram/CumulativeHistogram.h
+++ include/cumulative_histogram/CumulativeHistogram.h
@@ -867,40 +867,26 @@ namespace Detail_NS {
       const Entry& last_entry = path_.back();
       const std::size_t level_was = last_entry.level;
       if (last_entry.is_left_subtree) {
-        if (level_was == 1) {
+        // Special case - this is the only entry, and it represents the immediate left subtree of the root.
+        if (level_was == 1 && path_.size() == 1) {
           // Delete the last entry.
           path_.pop_back();
-          // Check if there is a previous entry at all.
-          if (path_.empty()) {
-            // Add an entry for the leftmost subtree of the immediate right subtree of the root.
-            // We know that the immediate right subtree of the root has 0 nodes, because the deleted
-            // entry was the immediate left subtree, but it was also a leaf, which means that the height
-            // of the right subtree cannot be greater than 1.
-            // PathEntry(1, 1) is the same as getRootEntry().rightChild().
-            path_.push_back(Entry{ .node = PathEntry(1, 1), .level = 1, .is_left_subtree = false });
-          }
-          else {
-            // Otherwise, the previous entry must be some right subtree of depth M.
-            assert(path_.back().is_left_subtree == false);
-            // Replace the previous entry with an entry for its immediate right subtree.
-            Entry& new_last_entry = path_.back();
-            new_last_entry.node.switchToRightChild();
-            // Increment the level from M to M+1.
-            ++new_last_entry.level;
-          }
+          // Add an entry for the leftmost subtree of the immediate right subtree of the root.
+          // We know that the immediate right subtree of the root has 0 nodes, because the deleted
+          // entry was the immediate left subtree, but it was also a leaf, which means that the height
+          // of the right subtree cannot be greater than 1.
+          // PathEntry(1, 1) is the same as getRootEntry().rightChild().
+          path_.push_back(Entry{ .node = PathEntry(1, 1), .level = 1, .is_left_subtree = false });
         }
         else {
-          // Remove the last entry for the K-th leftmost subtree of some node.
-          path_.pop_back();
-          // The entry we've just removed could not be the only entry in the path - if the last entry is
-          // the leftmost subtree at level K of the root, then K must be 1.
-          assert(!path_.empty());
-          // Add a new entry for the (K-1)-th leftmost subtree of the same node.
-          path_.push_back(Entry{ .node = path_.back().node.leftmostChild(level_was - 1),
-                                 .level = level_was - 1,
-                                 .is_left_subtree = true });
-          // Add another entry for the immediate right subtree of the node we've just added.
-          path_.push_back(Entry{ .node = path_.back().node.rightChild(), .level = 1, .is_left_subtree = false });
+          // The last entry is the leftmost subtree at level K of some node.
+          // If K > 1, then we replace it with an entry for the leftmost subtree at level (K-1) of the same node.
+          // Otherwise (if K == 1), we simply remove this entry.
+          switchToImmediateParent();
+          // Add another entry for the immediate right subtree of the new last entry.
+          addEntryForRightChild();
+          // Regardless of K, the new last entry should be nodeless now.
+          assert(path_.back().node.empty());
         }
       }
       else {
@@ -934,27 +920,15 @@ namespace Detail_NS {
         else {
           // Remove the last entry.
           path_.pop_back();
-          // The entry before the last must be the leftmost subtree at level M of some other node.
+          // The new last entry must be the leftmost subtree at level M of some other node.
           // That node cannot be the root, because that would mean that M == 0, and we don't store 0-level entries in the path.
-          assert(path_.size() >= 2);
-          assert(path_.back().is_left_subtree);
-          assert(!path_[path_.size() - 2].is_left_subtree);
-          const std::size_t level_m = path_.back().level;
-          if (level_m > 1) {
-            // Replace the entry for the leftmost subtree at level M with an entry for the leftmost subtree at level (M-1).
-            path_.pop_back();
-            path_.push_back(Entry{ .node = path_.back().node.leftmostChild(level_m - 1), .level = level_m - 1, .is_left_subtree = true });
-            // Add an entry for the immediate right subtree of the new last entry:
-            path_.push_back(Entry{ .node = path_.back().node.rightChild(), .level = 1, .is_left_subtree = false });
-          }
-          else {
-            // Remove the entry for the leftmost subtree at level M == 1.
-            path_.pop_back();
-            // The new last entry must be the rightmost subtree at level L of some node. We replace it with
-            // an entry for the rightmost subtree at level (L+1).
-            path_.back().node.switchToRightChild();
-            ++path_.back().level;
-          }
+          // We want to replace this entry with an entry for the leftmost subtree at level (M-1) if M > 1, or remove it altogether
+          // if M == 1.
+          switchToImmediateParent();
+          // If the last entry is a left subtree of some node, append an entry for its immediate right subtree.
+          // Otherwise, if the last entry is a right subtree at level L of some node, replace it with an entry
+          // for the right subtree at level L+1.
+          addEntryForRightChild();
           // If the previously added entry is not a leaf, add an entry for its deepest leftmost subtree.
           if (!path_.back().node.empty()) {
             path_.push_back(makeEntryForDeepestLeftmostSubtree(path_.back().node));
@@ -968,6 +942,49 @@ namespace Detail_NS {
     void popBack();
 
    private:
+    void switchToImmediateParent() noexcept {
+      // TODO: implement switching to the immediate parent for the root.
+      assert(!path_.empty());
+      const Entry& last_entry = path_.back();
+      const std::size_t last_entry_level = last_entry.level;
+      if (last_entry.is_left_subtree) {
+        if (last_entry_level > 1) {
+          // TODO: implement switching to the parent for the only entry in the path.
+          assert(path_.size() >= 2);
+          // Replace the entry for the leftmost subtree at level M with an entry for the leftmost subtree at level (M-1).
+          path_.pop_back();
+          path_.push_back(Entry{ .node = path_.back().node.leftmostChild(last_entry_level - 1),
+                                 .level = last_entry_level - 1, .is_left_subtree = true });
+        }
+        else {
+          // Remove the entry for the leftmost subtree at level M == 1.
+          path_.pop_back();
+        }
+      }
+      else {
+        // TODO: implement.
+        assert(false);
+      }
+    }
+
+    // If the last entry in the path is a left subtree of some node, appends an entry for its immediate right subtree.
+    // Otherwise, if the last entry is a right subtree at level L of some node, replaces it with an entry
+    // for the right subtree at level L+1.
+    void addEntryForRightChild() noexcept {
+      // TODO: implement adding an entry for the immediate right subtree of the root.
+      assert(!path_.empty());
+      Entry& last_entry = path_.back();
+      // The last entry must not be for a leaf node.
+      assert(!last_entry.node.empty());
+      if (last_entry.is_left_subtree) {
+        path_.push_back(Entry{ .node = last_entry.node.rightChild(), .level = 1, .is_left_subtree = false });
+      }
+      else {
+        last_entry.node.switchToRightChild();
+        ++last_entry.level;
+      }
+    }
+
     // Constructs an entry for the deepest leftmost subtree of the given PathEntry.
     // The behavior is unspecified if path_entry.empty().
     // \return an entry for the deepest leftmost subtree of path_entry.
